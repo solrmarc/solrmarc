4029e0a4be9285251d48eca4bfa79f716c3d6a59
rh9ec <rh9ec@virginia.edu>
2008-06-18 16:42:21 +0000

    fix for new footprint of MarcTranslatedReader

--- src/ZClient.java
+++ src/ZClient.java
@@ -1,27 +1,19 @@
-
-
-
-//
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either version 2.1 of
-//the license, or (at your option) any later version.
-//
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-//
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 59 Temple Place - Suite
-//330, Boston, MA  02111-1307, USA.
-//
-
-//import java.util.*;
-
-//import com.k_int.gen.Z39_50_APDU_1995.*;
-//import org.jzkit.a2j.gen.AsnUseful.*;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import java.io.ByteArrayInputStream;
 import java.util.Enumeration;
@@ -279,7 +271,7 @@ public class ZClient extends SynchronousOriginBean
         System.out.println("made ByteArrayInputStream");
         MarcStreamReader m1 = new MarcStreamReader(bs);
         System.out.println("made MarcStreamReader");
-        MarcTranslatedReader mr = new MarcTranslatedReader(m1);
+        MarcTranslatedReader mr = new MarcTranslatedReader(m1, true);
         System.out.println("made MarcTranslatedReader");
         String result = null;
         System.out.println("checking for next");
