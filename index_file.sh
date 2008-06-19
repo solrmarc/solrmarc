ebf192114e663177c7d14d4f5a1597b1f7b48e22
wayne.graham <wayne.graham@31eaa32e-b84e-0410-9ca4-7366b6a8ecef>
2008-06-19 14:02:46 +0000

    Removed Windows newline symbol (^M) from index_file script.

--- index_file.sh
+++ index_file.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#! /bin/sh
+
 file=$1
-java -Xmx1024M -Dmarc.path="$file" -Dsolr.optimize_at_end="false" -Dsolr.hosturl="" -jar dist/MarcImporter.jar $2
+java -Xmx1024m -Dmarc.path="$file" -Dsolr.optimize_at_end="false" -Dsolr.hosturl="" -jar dist/MarcImporter.jar $2
