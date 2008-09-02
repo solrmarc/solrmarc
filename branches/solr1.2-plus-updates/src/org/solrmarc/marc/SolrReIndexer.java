e34d98ea3ca5580049c4662bafecba9792ee5bc1
rh9ec@virginia.edu <rh9ec@virginia.edu@31eaa32e-b84e-0410-9ca4-7366b6a8ecef>
2008-09-02 21:18:16 +0000

    Make it work with BooklistReader

--- branches/solr1.2-plus-updates/src/org/solrmarc/marc/SolrReIndexer.java
+++ branches/solr1.2-plus-updates/src/org/solrmarc/marc/SolrReIndexer.java
@@ -254,6 +254,10 @@ public class SolrReIndexer
                     {
                         update(docMap);
                     }
+                    else
+                    {
+                        return(docMap);
+                    }
                 }
             }
         }
