0dd9c2f15c3d579978b2ba436306b51ed09b5950
naomi.dushay <naomi.dushay@31eaa32e-b84e-0410-9ca4-7366b6a8ecef>
2009-01-23 21:18:23 +0000

    stanford vufind indexer for solr 1.2, with test code

---
+++ branches/solrmarc-2.0/site-specific-examples/stanfordVuFind/test/src/edu/stanford/BibIndexTest.java
@@ -0,0 +1,57 @@
+package edu.stanford;
+
+import java.io.*;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import org.solrmarc.index.IndexTest;
+
+/**
+ * Site Specific code used for testing the stanford Bibliographic index
+ * @author Naomi Dushay
+ *
+ */
+public abstract class BibIndexTest extends IndexTest {
+
+	// Note:  the hardcodings below are only used when the tests are
+	//  invoked without the properties set
+	//   the properties ARE set when the tests are invoke via ant.
+	{
+		String parentDir = File.separator + "Users" + File.separator + "naomi" + File.separator + "NGDE";
+		
+		solrmarcPath = System.getProperty("solrmarc.path");
+		if (solrmarcPath == null)
+			solrmarcPath = parentDir + File.separator + "solrmarc" + File.separator + "stanfordSolrmarc2.0";
+		
+		siteSpecificPath = System.getProperty("solrmarc.site.path");
+		if (siteSpecificPath == null)
+			siteSpecificPath = solrmarcPath + File.separator + "site-specific-examples" + File.separator + "stanfordVuFind"; 
+		
+		configPropFile = siteSpecificPath + File.separator + "stanford_config.properties";
+		
+		solrPath = System.getProperty("solr.path");
+		if (solrPath == null)
+			solrPath = parentDir +  File.separator + "vufind" + File.separator + "vufind - local svn" +  File.separator + "solr";
+
+		testDir = solrmarcPath + File.separator + "test";
+		testDataParentPath = testDir + File.separator + "data";
+		testDataPath = testDataParentPath + File.separator + "allfieldsTests.mrc";
+		solrDataDir = System.getProperty("solr.data.dir");
+		if (solrDataDir == null)
+			solrDataDir = testDir + File.separator + "solr" + File.separator + "data";
+	}
+
+	
+	public void createIxInitVars(String testDataFname) 
+		throws ParserConfigurationException, IOException, SAXException 
+	{
+		createNewTestIndex(testDataParentPath + File.separator + testDataFname, configPropFile, solrPath, solrDataDir, solrmarcPath, siteSpecificPath);
+		solrCore = getSolrCore(solrPath, solrDataDir);
+		sis = getSolrIndexSearcher(solrCore);
+	}
+
+	
+
+}
