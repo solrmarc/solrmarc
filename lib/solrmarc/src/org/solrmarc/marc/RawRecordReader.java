d0aa86f57e463ce20be6416224d4c2a3c3fc3938
rh9ec <rh9ec@virginia.edu>
2009-03-18 21:49:02 +0000

    Added ability to pass in an id regex or a filename containing ids

--- lib/solrmarc/src/org/solrmarc/marc/RawRecordReader.java
+++ lib/solrmarc/src/org/solrmarc/marc/RawRecordReader.java
@@ -1,14 +1,18 @@
 package org.solrmarc.marc;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 import org.apache.log4j.Logger;
 
@@ -45,122 +49,91 @@ public class RawRecordReader
     public static void main(String[] args)
     {
     //    try {
-        if (args.length == 2)
+        DataInputStream input;
+        if (args.length < 2)
         {
-            String idLookedFor = args[1].trim();
-            byte[] byteArray = new byte[24];
-            try
+            System.err.println("Error: No records specified for extraction");
+        }
+        try
+        {
+            if (args[0].equals("-"))
             {
-                DataInputStream input;
-                if (args[0].equals("-"))
-                {
-                    input = new DataInputStream(new BufferedInputStream(System.in));
-                }
-                else
-                {    
-                    input = new DataInputStream(new BufferedInputStream(new FileInputStream(new File(args[0]))));
-                }
-                while (true)
-                {
-                    input.readFully(byteArray);
-                    int recordLength = parseRecordLength(byteArray);
-                    byte[] recordBuf = new byte[recordLength - 24];
-                    input.readFully(recordBuf);
-                    String recordStr = null;
-                    try
-                    {
-                        recordStr = new String(recordBuf, "ISO-8859-1");
-                    }
-                    catch (UnsupportedEncodingException e)
-                    {
-                        // e.printStackTrace();
-                        logger.error(e.getMessage());
-                    }
-                    if (recordStr.startsWith("001"))
-                    {
-                        String leader = new String(byteArray);
-                        int offset = Integer.parseInt(leader.substring(12,17)) - 24;
-                        int length = Integer.parseInt(recordStr.substring(3,7));
-                        int offset2 = Integer.parseInt(recordStr.substring(7,12));
-                        String id = recordStr.substring(offset+offset2, offset+offset2+length-1);
-                        if (id.trim().equals(idLookedFor))
-                        { 
-                            System.out.write(byteArray);
-                            System.out.write(recordBuf);
-                            System.out.flush();
-                        }
-                    }
-    
-                }
+                input = new DataInputStream(new BufferedInputStream(System.in));
             }
-            catch (EOFException e)
+            else
+            {    
+                input = new DataInputStream(new BufferedInputStream(new FileInputStream(new File(args[0]))));
+            }            
+            if (!args[1].equals(".txt"))
             {
+                String idRegex = args[1].trim();
+                processInput(input, idRegex, null);
             }
-            catch (IOException e)
+            else 
             {
-                //  e.printStackTrace();
-                logger.error(e.getMessage());
+                File idList = new File(args[1]);
+                BufferedReader idStream = new BufferedReader(new InputStreamReader(new BufferedInputStream(new FileInputStream(idList))));
+                String line;
+                String findReplace[] = null;
+                if (args.length > 2) findReplace = args[2].split("->");
+                LinkedHashSet<String> idsLookedFor = new LinkedHashSet<String>();
+                while ((line = idStream.readLine()) != null)
+                {
+                    if (findReplace != null)
+                    {
+                        line = line.replaceFirst(findReplace[0], findReplace[1]);
+                    }
+                    idsLookedFor.add(line);
+                }
+                processInput(input, null, idsLookedFor);
+
             }
         }
-        else if (args.length == 3)
+        catch (EOFException e)
         {
-            String idStart = args[1].trim();
-            String idEnd = args[2].trim();
-            int idMin = Integer.parseInt(idStart.substring(1));
-            int idMax = Integer.parseInt(idEnd.substring(1));
-            byte[] byteArray = new byte[24];
+            //  Done Reading input,   Be happy
+        }
+        catch (IOException e)
+        {
+            //  e.printStackTrace();
+            logger.error(e.getMessage());
+        }
+
+    }
+
+    static void processInput(DataInputStream input, String idRegex, HashSet<String>idsLookedFor) throws IOException
+    {
+        byte[] byteArray = new byte[24];
+        while (true)
+        {
+            input.readFully(byteArray);
+            int recordLength = parseRecordLength(byteArray);
+            byte[] recordBuf = new byte[recordLength - 24];
+            input.readFully(recordBuf);
+            String recordStr = null;
             try
             {
-                DataInputStream input;
-                if (args[0].equals("-"))
-                {
-                    input = new DataInputStream(new BufferedInputStream(System.in));
-                }
-                else
-                {    
-                    input = new DataInputStream(new BufferedInputStream(new FileInputStream(new File(args[0]))));
-                }
-                while (true)
-                {
-                    input.readFully(byteArray);
-                    int recordLength = parseRecordLength(byteArray);
-                    byte[] recordBuf = new byte[recordLength - 24];
-                    input.readFully(recordBuf);
-                    String recordStr = null;
-                    try
-                    {
-                        recordStr = new String(recordBuf, "ISO-8859-1");
-                    }
-                    catch (UnsupportedEncodingException e)
-                    {
-                        // e.printStackTrace();
-                        logger.error(e.getMessage());
-                    }
-                    if (recordStr.startsWith("001"))
-                    {
-                        String leader = new String(byteArray);
-                        int offset = Integer.parseInt(leader.substring(12,17)) - 24;
-                        int length = Integer.parseInt(recordStr.substring(3,7));
-                        int offset2 = Integer.parseInt(recordStr.substring(7,12));
-                        String id = recordStr.substring(offset+offset2, offset+offset2+length-1);
-                        int idAsInt = Integer.parseInt(id.substring(1));
-                        if ( idAsInt >= idMin && idAsInt < idMax)
-                        { 
-                            System.out.write(byteArray);
-                            System.out.write(recordBuf);
-                            System.out.flush();
-                        }
-                    }
-    
-                }
+                recordStr = new String(recordBuf, "ISO-8859-1");
             }
-            catch (EOFException e)
+            catch (UnsupportedEncodingException e)
             {
-            }
-            catch (IOException e)
-            {
-                //  e.printStackTrace();
+                // e.printStackTrace();
                 logger.error(e.getMessage());
+            }
+            if (recordStr.startsWith("001"))
+            {
+                String leader = new String(byteArray);
+                int offset = Integer.parseInt(leader.substring(12,17)) - 24;
+                int length = Integer.parseInt(recordStr.substring(3,7));
+                int offset2 = Integer.parseInt(recordStr.substring(7,12));
+                String id = recordStr.substring(offset+offset2, offset+offset2+length-1).trim();
+                if ( (idsLookedFor == null && id.matches(idRegex)) ||
+                     (idsLookedFor != null && idsLookedFor.contains(id) ) )
+                { 
+                    System.out.write(byteArray);
+                    System.out.write(recordBuf);
+                    System.out.flush();
+                }
             }
         }
     }
