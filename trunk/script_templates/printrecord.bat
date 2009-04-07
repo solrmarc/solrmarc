dc35b39b18e6dd50fdb9e818c21bf439d488d385
rh9ec@virginia.edu <rh9ec@virginia.edu@31eaa32e-b84e-0410-9ca4-7366b6a8ecef>
2009-04-07 17:38:09 +0000

    removed memargs declaration since these programs don't load solr and don't use much memory.

--- trunk/script_templates/printrecord.bat
+++ trunk/script_templates/printrecord.bat
@@ -8,4 +8,4 @@ for %%x in (%0) do set scriptdir=%%~dpsx
 for %%x in (%scriptdir%) do set scriptdir=%%~dpsx
 ::echo BatchPath = %scriptdir%
 
-java @MEM_ARGS@ -Done-jar.main.class="org.solrmarc.marc.MarcPrinter" -jar %scriptdir%@CUSTOM_JAR_NAME@ print %1 %2 %3
+java -Done-jar.main.class="org.solrmarc.marc.MarcPrinter" -jar %scriptdir%@CUSTOM_JAR_NAME@ print %1 %2 %3
