445853655e4892f818e190e99c121d7388d75a21
rh9ec <rh9ec@virginia.edu>
2009-04-15 20:11:56 +0000

    change how solr war file or solr jars are set when built with solr.version set to external (including all pre-built binaries)

--- script_templates/indextest2.bat
+++ script_templates/indextest2.bat
@@ -9,7 +9,7 @@ for %%x in (%0) do set scriptdir=%%~dpsx
 for %%x in (%scriptdir%) do set scriptdir=%%~dpsx
 ::echo BatchPath = %scriptdir%
 ::
-if (%SOLRMARC_MEM_ARGS%) EQU ()  set SOLRMARC_MEM_ARGS=@MEM_ARGS@
+if "%SOLRMARC_MEM_ARGS%" EQU ""  set SOLRMARC_MEM_ARGS=@MEM_ARGS@
 ::
 java %SOLRMARC_MEM_ARGS% %solrjardef% -Dmarc.just_index_dont_add="true" -jar %scriptdir%@CUSTOM_JAR_NAME@ %1 %2 %3 
 
