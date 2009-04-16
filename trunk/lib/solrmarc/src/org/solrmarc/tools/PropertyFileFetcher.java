081738a5231a75cdadf195b697c8966d920a747f
rh9ec@virginia.edu <rh9ec@virginia.edu@31eaa32e-b84e-0410-9ca4-7366b6a8ecef>
2009-04-16 22:25:37 +0000

    changes to allow all calls of jar to be removed from the batch files and shell scripts

--- trunk/lib/solrmarc/src/org/solrmarc/tools/PropertyFileFetcher.java
+++ trunk/lib/solrmarc/src/org/solrmarc/tools/PropertyFileFetcher.java
@@ -1,6 +1,8 @@
 package org.solrmarc.tools;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,28 +13,58 @@ public class PropertyFileFetcher {
 	 */
 	public static void main(String[] args) 
 	{
-		String propertyFile = null;
-		if (args.length > 0 && args[0].endsWith("properties")) 
-		{
-			propertyFile = args[0];
-		}
-		if (propertyFile == null) 
-		{
-			propertyFile = GetDefaultConfig.getConfigName("");
-		}
-		InputStream in = Utils.getPropertyFileInputStream(null, propertyFile);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		String line;
-		try {
-			while ((line = reader.readLine()) != null) 
-			{
-				System.out.println(line);
-			}
-			reader.close();
-		} 
-		catch (IOException e) 
-		{
-		    System.err.println("Error: Exception occurred reading specified file "+ propertyFile+ " from jar");
-		}
+		
+        if (args.length > 0 && args[0].endsWith("jar")) 
+        {
+            byte[] buffer = new byte[1024];
+            int    bytesRead;
+            String jarFile = args[0];
+            String directory = ".";
+            if (args.length > 1)
+            {
+                directory = args[1]; 
+            }
+            InputStream in = Utils.getPropertyFileInputStream(null, jarFile);
+            File outputFile = new File(directory, jarFile);
+            try {
+                FileOutputStream out = new FileOutputStream(outputFile);
+                while ((bytesRead = in.read(buffer)) != -1) 
+                {
+                    out.write(buffer, 0, bytesRead);
+                }
+                out.close();
+                in.close();
+            }
+            catch (IOException ioe)
+            {
+                System.err.println("Error: extracting jar file");
+                System.exit(1);
+            }
+            System.exit(0);
+        }
+        
+        String propertyFile = null;
+        if (args.length > 0 && args[0].endsWith("properties")) 
+        {
+            propertyFile = args[0];
+        }
+        if (propertyFile == null) 
+        {
+            propertyFile = GetDefaultConfig.getConfigName("");
+        }
+        InputStream in = Utils.getPropertyFileInputStream(null, propertyFile);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) 
+            {
+                System.out.println(line);
+            }
+            reader.close();
+        } 
+        catch (IOException e) 
+        {
+            System.err.println("Error: Exception occurred reading specified file "+ propertyFile+ " from jar");
+        }
 	}
 }
