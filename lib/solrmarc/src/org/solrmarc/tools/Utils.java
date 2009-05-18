ac88da9ec62c6d1d70c7d72be7f3bbed3b618082
rh9ec <rh9ec@virginia.edu>
2009-05-18 14:39:22 +0000

    Greatly expanded the cleanDate routine to do a better job of finding and extracting dates from 260c field.

--- lib/solrmarc/src/org/solrmarc/tools/Utils.java
+++ lib/solrmarc/src/org/solrmarc/tools/Utils.java
@@ -27,6 +27,7 @@ import org.apache.log4j.Appender;
 import org.apache.log4j.AsyncAppender;
 import org.apache.log4j.Logger;
 import org.marc4j.marc.*;
+import org.solrmarc.marc.MarcImporter;
 
 /**
  * General utility functions for solrmarc
@@ -37,10 +38,22 @@ import org.marc4j.marc.*;
 
 public final class Utils {
 	
-	private final static Pattern FOUR_DIGIT_PATTERN = Pattern.compile("\\d{1,4}");
-	private static Matcher matcher;
+    private final static Pattern FOUR_DIGIT_PATTERN_BRACES = Pattern.compile("\\[[12]\\d{3,3}\\]");
+    private final static Pattern FOUR_DIGIT_PATTERN_ONE_BRACE = Pattern.compile("\\[[12]\\d{3,3}");
+    private final static Pattern FOUR_DIGIT_PATTERN_STARTING_WITH_1_2 = Pattern.compile("[12]\\d{3,3}");
+    private final static Pattern FOUR_DIGIT_PATTERN_OTHER_1 = Pattern.compile("l\\d{3,3}");
+    private final static Pattern FOUR_DIGIT_PATTERN_OTHER_2 = Pattern.compile("\\[19\\]\\d{2,2}");
+    private final static Pattern FOUR_DIGIT_PATTERN_OTHER_3 = Pattern.compile("[12][7890][0-9][-?0-9]");
+    private final static Pattern FOUR_DIGIT_PATTERN = Pattern.compile("\\d{4,4}");
+    private static Matcher matcher;
+    private static Matcher matcher_braces;
+    private static Matcher matcher_one_brace;
+    private static Matcher matcher_start_with_1_2;
+    private static Matcher matcher_l_plus_three_digits;
+    private static Matcher matcher_bracket_19_plus_two_digits;
+    private static Matcher matcher_three_digits_plus_unk;
 	private final static DecimalFormat timeFormat = new DecimalFormat("00.00");
-	
+    protected static Logger logger = Logger.getLogger(Utils.class.getName());	
 	/**
 	 * Default Constructor
 	 * It's private, so it can't be instantiated by other objects
@@ -179,14 +192,66 @@ public final class Utils {
 	 */
 	public static String cleanDate(final String date)
 	{
-		matcher = FOUR_DIGIT_PATTERN.matcher(date);
+        matcher_braces = FOUR_DIGIT_PATTERN_BRACES.matcher(date);
+        matcher_one_brace = FOUR_DIGIT_PATTERN_ONE_BRACE.matcher(date);
+        matcher_start_with_1_2 = FOUR_DIGIT_PATTERN_STARTING_WITH_1_2.matcher(date);
+        matcher_l_plus_three_digits = FOUR_DIGIT_PATTERN_OTHER_1.matcher(date);
+        matcher_bracket_19_plus_two_digits = FOUR_DIGIT_PATTERN_OTHER_2.matcher(date);
+        matcher_three_digits_plus_unk = FOUR_DIGIT_PATTERN_OTHER_3.matcher(date);
+        matcher = FOUR_DIGIT_PATTERN.matcher(date);
 		
 		String cleanDate = null; // raises DD-anomaly
 		
-		if(matcher.find()){	
-			cleanDate = matcher.group();
-		} 
-		
+        if(matcher_braces.find())
+        {   
+            cleanDate = matcher_braces.group();
+            cleanDate = Utils.removeOuterBrackets(cleanDate);
+            if (matcher.find())
+            {
+                String tmp = matcher.group();
+                if (!tmp.equals(cleanDate))
+                {
+                    tmp = "" + tmp;
+                }
+            }
+        } 
+        else if(matcher_one_brace.find())
+        {   
+            cleanDate = matcher_one_brace.group();
+            cleanDate = Utils.removeOuterBrackets(cleanDate);
+            if (matcher.find())
+            {
+                String tmp = matcher.group();
+                if (!tmp.equals(cleanDate))
+                {
+                    tmp = "" + tmp;
+                }
+            }
+        } 
+        else if(matcher_start_with_1_2.find())
+        {   
+            cleanDate = matcher_start_with_1_2.group();
+        } 
+        else if(matcher_l_plus_three_digits.find())
+        {   
+            cleanDate = matcher_l_plus_three_digits.group().replaceAll("l", "1");
+        } 
+        else if(matcher_bracket_19_plus_two_digits.find())
+        {   
+            cleanDate = matcher_bracket_19_plus_two_digits.group().replaceAll("\\[", "").replaceAll("\\]", "");
+        } 
+        else if(matcher_three_digits_plus_unk.find())
+        {   
+            cleanDate = matcher_three_digits_plus_unk.group().replaceAll("[-?]", "0");
+        } 
+		if (cleanDate != null)
+		{
+            logger.info("Date : "+ date + " mapped to : "+ cleanDate);		    
+		}
+		else
+		{
+		    logger.info("No Date match: "+ date);
+		}
 		return cleanDate;
 	}
     
