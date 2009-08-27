a50851c4f1a05a7d709c4e474251ec3fdaffa1df
naomi.dushay <naomi.dushay@31eaa32e-b84e-0410-9ca4-7366b6a8ecef>
2009-08-27 18:23:43 +0000

    smoke test for GenericBlacklight actually works!  Yay us!

--- trunk/examples/GenericBlacklight/test/src/org/blacklight/SmokeTests.java
+++ trunk/examples/GenericBlacklight/test/src/org/blacklight/SmokeTests.java
@@ -8,62 +8,47 @@ import org.xml.sax.SAXException;
 
 import org.junit.*;
 
-import static org.junit.Assert.*;
-
-
 /**
- * junit4 tests for blacklight demo use of solrmarc
+ * junit4 tests for generic blacklight example
+ * 
  * @author Naomi Dushay
  */
-public class SmokeTests extends DemoIxTest {
-	
-	
-@Before
-	public final void setup() 
-			throws ParserConfigurationException, IOException, SAXException 
-	{
-		createIxInitVars("test_data.utf8.mrc");
-	}
-	
-	/**
-	 * Test populate of id field
-	 */
-@Test 
-	public final void testId() 
-		throws ParserConfigurationException, SAXException, IOException
-	{
-		String fldName = "id";
-		assert
-		assertStringFieldProperties(fldName, solrCore, sis);
-		assertFieldNotMultiValued(fldName, solrCore);		
-		assertFieldStored(fldName, solrCore);
-		assertFieldIndexed(fldName, solrCore);
-		
- //       int numDocs = sis.getReader().numDocs();
- //       assertEquals("Number of documents in index incorrect: ", 3, numDocs);
-        assertDocNotPresent("001noSubNo004", sis);
-        assertDocPresent("001suba", sis);
-        assertDocNotPresent("001and004nosub", sis);
-        assertDocNotPresent("004noSuba", sis);
-        assertDocPresent("001subaAnd004nosub", sis);
-        assertDocNotPresent("004noSuba", sis);
-        assertDocPresent("001subaAnd004suba", sis);
-        assertDocNotPresent("004suba", sis);
-        
-        assertSingleResult("001suba", fldName, "\"001suba\"", sis);
-        assertSingleResult("001subaAnd004nosub", fldName, "\"001subaAnd004nosub\"", sis);
-        assertSingleResult("001subaAnd004suba", fldName, "\"001subaAnd004suba\"", sis);
-	}
-	
+public class SmokeTests extends org.blacklight.DemoIxTest
+{
 
-	/**
-	 * Hokey horrible way to create a test index.
-	 */
-//@Test
-    public final void makeIndex() 
-			throws ParserConfigurationException, IOException, SAXException 
-	{
-		createIxInitVars("unicornWHoldings.mrc");
-	}
+    @Before
+    public final void setup() throws ParserConfigurationException, IOException,
+            SAXException
+    {
+        createIxInitVars("test_data.utf8.mrc");
+    }
+
+    /**
+     * Test id field
+     */
+    @Test
+    public final void testId() throws ParserConfigurationException,
+            SAXException, IOException
+    {
+        String fldName = "id";
+        assertStringFieldProperties(fldName);
+        assertFieldNotMultiValued(fldName);
+        assertFieldStored(fldName);
+        assertFieldIndexed(fldName);
+
+        assertDocPresent("00282214");
+
+        assertSingleResult("00282214", fldName, "00282214");
+    }
+
+    /**
+     * Hokey horrible way to create a test index.
+     */
+    // @Test
+    public final void makeIndex() throws ParserConfigurationException,
+            IOException, SAXException
+    {
+        createIxInitVars("unicornWHoldings.mrc");
+    }
 
 }
