ab223db054e447a0c45ab07b2d70229d146c8839
rh9ec <rh9ec@virginia.edu>
2009-09-18 19:08:06 +0000

    Refactored code so that the static method getConfigName()  still operates the same, but some of what it does is not performed by a separately callable static method named getJarFileName()

--- lib/solrmarc/src/org/solrmarc/tools/GetDefaultConfig.java
+++ lib/solrmarc/src/org/solrmarc/tools/GetDefaultConfig.java
@@ -1,13 +1,6 @@
 package org.solrmarc.tools; 
-import java.io.BufferedReader; 
-import java.io.DataOutputStream; 
 import java.io.IOException; 
-import java.io.InputStreamReader; 
-import java.net.MalformedURLException; 
-import java.net.URL; 
-import java.net.URLConnection; 
 import java.util.Enumeration; 
-import java.util.Properties; 
 import java.util.jar.JarFile; 
 import java.util.jar.Manifest; 
 import java.util.zip.ZipEntry; 
@@ -29,35 +22,59 @@ public class GetDefaultConfig
      * @throws IOException 
      */
 
+    public static String getJarFileName()
+    { 
+        String jarFilename = null; 
+        Class<?> bootClass;
+        try
+        {
+            bootClass = Class.forName("com.simontuffs.onejar.Boot");
+            jarFilename = bootClass.getMethod("getMyJarPath").invoke(null).toString();
+        }
+        catch (Exception e)
+        {
+            // Program not running from within a OneJar 
+            jarFilename = null;
+        }
+        return(jarFilename);
+    }
+    
+    /** 
+     * Extract the manifest attribute Default-Config-File from the top level jar file 
+     * @throws IOException 
+     */
+
     public static String getConfigName(String defaultValue)
     { 
         String configProperties = null; 
-        try { 
-            Class<?> bootClass = Class.forName("com.simontuffs.onejar.Boot"); 
-            String jar = bootClass.getMethod("getMyJarPath").invoke(null).toString(); 
-            JarFile jarFile = new JarFile(jar); 
-            Manifest manifest = jarFile.getManifest(); 
-            String defConfig = manifest.getMainAttributes().getValue("Default-Config-File"); 
-            if (defConfig != null && defConfig.length() > 0) 
-            { 
-                configProperties = defConfig; 
-            } 
-            else 
-            { 
-                Enumeration entries = jarFile.entries(); 
-                while (entries.hasMoreElements()) 
+        String jar = getJarFileName();
+        if (jar != null)
+        {
+            try { 
+                JarFile jarFile = new JarFile(jar); 
+                Manifest manifest = jarFile.getManifest(); 
+                String defConfig = manifest.getMainAttributes().getValue("Default-Config-File"); 
+                if (defConfig != null && defConfig.length() > 0) 
                 { 
-                    ZipEntry entry = (ZipEntry)entries.nextElement(); 
-                    if (entry.getName().contains("config.properties")) 
+                    configProperties = defConfig; 
+                } 
+                else 
+                { 
+                    Enumeration entries = jarFile.entries(); 
+                    while (entries.hasMoreElements()) 
                     { 
-                        configProperties = entry.getName(); 
+                        ZipEntry entry = (ZipEntry)entries.nextElement(); 
+                        if (entry.getName().contains("config.properties")) 
+                        { 
+                            configProperties = entry.getName(); 
+                        } 
                     } 
                 } 
             } 
-        } 
-        catch (Exception e) 
-        { 
-            // no manifest property defining the config 
+            catch (Exception e) 
+            { 
+                // no manifest property defining the config 
+            }
         }
         if (configProperties == null)
         {
