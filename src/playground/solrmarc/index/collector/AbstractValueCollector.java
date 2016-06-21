c184727c0eefe0ba7dfc570d71f578dfc280945d
Robert Haschart <rh9ec@virginia.edu>
2016-06-21 18:27:26 -0400

    Removing no-longer-used classes

--- src/playground/solrmarc/index/collector/AbstractValueCollector.java
+++
@@ -1,9 +0,0 @@
-package playground.solrmarc.index.collector;
-
-import java.util.Collection;
-
-public interface AbstractValueCollector<T>
-{
-    // Object collect(final T values);
-    Collection<String> collect(final T values) throws Exception;
-}
