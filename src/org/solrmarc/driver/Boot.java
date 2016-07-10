7a04011536417fd11de1ec23c7aea3cc8b725ea3
rh9ec <rh9ec@virginia.edu>
2016-07-10 00:11:19 -0400

    Change boot class to use Logger rather than System.err.println

--- src/org/solrmarc/driver/Boot.java
+++ src/org/solrmarc/driver/Boot.java
@@ -12,77 +12,84 @@ import java.net.URLClassLoader;
 import java.security.CodeSource;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.solrmarc.index.utils.ReflectionUtils;
 
 public class Boot
 {
-    static {
+    public static Logger logger;
+
+    static
+    {
         addLibDirJarstoClassPath();
+        logger = Logger.getLogger(Boot.class);
     }
-    
+
     public static void main(String[] args)
     {
-        if (args.length == 0) 
+        if (args.length == 0)
         {
             findExecutables();
         }
         else
         {
-            String[] otherArgs = new String[args.length-1];
-            System.arraycopy(args, 1,  otherArgs,  0,  args.length-1);
+            String[] otherArgs = new String[args.length - 1];
+            System.arraycopy(args, 1, otherArgs, 0, args.length - 1);
             try
             {
                 Class<?> mainClass = Class.forName(args[0]);
                 Method mainMethod = mainClass.getMethod("main", String[].class);
                 if (!Modifier.isStatic(mainMethod.getModifiers()))
                 {
-                    System.err.println("Main method is not static in class: "+ args[0]);
+                    logger.fatal("ERROR: Main method is not static in class: " + args[0]);
                     System.exit(1);
                 }
-                mainMethod.invoke(null, (Object)otherArgs);
+                mainMethod.invoke(null, (Object) otherArgs);
             }
             catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
             {
-                System.err.println("Unable to invoke main method in specified main: "+ args[0]);
+                logger.fatal("ERROR: Unable to invoke main method in specified main: " + args[0]);
                 System.err.println(e.getMessage());
                 System.exit(2);
             }
             catch (ClassNotFoundException e)
             {
-                System.err.println("Unable to find specified main class: "+ args[0]);
+                logger.fatal("ERROR: Unable to find specified main class: " + args[0]);
+                findExecutables();
                 System.exit(3);
             }
             catch (NoSuchMethodException e)
             {
-                System.err.println("Unable to find main method in specified class: "+ args[0]);
+                logger.fatal("ERROR: Unable to find main method in specified class: " + args[0]);
                 System.exit(4);
             }
             catch (SecurityException e)
             {
-                System.err.println("Unable to access main method in specified class: "+ args[0]);
+                logger.fatal("ERROR: Unable to access main method in specified class: " + args[0]);
                 System.exit(5);
             }
         }
     }
-        
+
     private static void findExecutables()
     {
         Set<Class<? extends Boot>> mainClasses = ReflectionUtils.getBootableMainClasses();
-        
-        System.out.println("This class  org.solrmarc.driver.Boot  dynamically loads all of the jars in the lib directory");
-        System.out.println("and then calls the main method of a class that requires those jars");
-        System.out.println("The first argument provided to this class specifies the name of the class to load and execute");
-        System.out.println("all of the subsequent arguments are then passed to that class's main method");
 
-        System.out.println("");
-        System.out.println("Known classes that can be bootstrapped in tha manner are:");
+        logger.info("This class  org.solrmarc.driver.Boot  dynamically loads all of the jars in the lib directory");
+        logger.info("and then calls the main method of a class that requires those jars");
+        logger.info("The first argument provided to this class specifies the name of the class to load and execute");
+        logger.info("all of the subsequent arguments are then passed to that class's main method");
+
+        logger.info("");
+        logger.info("Known classes that can be bootstrapped in tha manner are:");
         for (Class<?> mainClass : mainClasses)
         {
-           try { 
+            try
+            {
                 Method mainMethod = mainClass.getMethod("main", String[].class);
                 if (Modifier.isStatic(mainMethod.getModifiers()))
                 {
-                    System.out.println("    "+mainClass.getName());
+                    logger.info("    " + mainClass.getName());
                 }
             }
             catch (NoSuchMethodException e)
@@ -90,8 +97,6 @@ public class Boot
                 // no prob
             }
         }
-        
-        
     }
 
     /**
@@ -113,11 +118,12 @@ public class Boot
         }
         if (jarFile.getName().endsWith(".jar"))
         {
-            jarDir = jarFile.getParentFile().getPath();        
+            jarDir = jarFile.getParentFile().getPath();
         }
         else
         {
-            // Not running from a jar.  Probably running from eclipse or other IDE
+            // Not running from a jar. Probably running from eclipse or other
+            // IDE
             jarDir = new File(".").getAbsoluteFile().getParentFile().getAbsolutePath();
         }
         File libPath = new File(jarDir, "lib");
@@ -127,21 +133,21 @@ public class Boot
         }
         catch (RuntimeException ise)
         {
-            System.err.println("Fatal error: Failure while loading jars from lib directory"+ ise.getMessage());
+            logger.fatal("Fatal error: Failure while loading jars from lib directory" + ise.getMessage());
             System.exit(10);
         }
-        
+
         try
         {
             Class.forName("org.marc4j.marc.Record");
         }
         catch (ClassNotFoundException e)
         {
-            System.err.println("Fatal error: Unable to find marc4j Record class, probably missing many others as well."+ e.getMessage());
+            logger.fatal("Fatal error: Unable to find marc4j Record class, probably missing many others as well." + e.getMessage());
             System.exit(11);
         }
     }
-    
+
     public static void extendClasspathWithJar(URLClassLoader sysLoader, File jarfile)
     {
         URL urls[] = sysLoader.getURLs();
@@ -152,7 +158,8 @@ public class Boot
         }
         catch (MalformedURLException e)
         {
-            // This shouldn't happen since the jarfile is passed in as a file from a directory
+            // This shouldn't happen since the jarfile is passed in as a file
+            // from a directory
             e.printStackTrace();
             return;
         }
@@ -163,12 +170,13 @@ public class Boot
             if (urls[i].toString().equalsIgnoreCase(ujars)) return;
         }
         Class<URLClassLoader> sysClass = URLClassLoader.class;
-        try {
-            Method method = sysClass.getDeclaredMethod("addURL", new Class[]{URL.class});
+        try
+        {
+            Method method = sysClass.getDeclaredMethod("addURL", new Class[] { URL.class });
             method.setAccessible(true);
-            method.invoke(sysLoader, new Object[] {ujar});
-        } 
-        catch (Throwable t) 
+            method.invoke(sysLoader, new Object[] { ujar });
+        }
+        catch (Throwable t)
         {
             t.printStackTrace();
         }
@@ -188,7 +196,7 @@ public class Boot
             {
                 dirpath = dir.getAbsolutePath();
             }
-            throw new RuntimeException("Unable to find Jars for SolrJ in the provided directory: "+dirpath );
+            throw new RuntimeException("Unable to find any Jars in the provided directory: " + dirpath);
         }
         for (File file : dir.listFiles())
         {
@@ -196,7 +204,6 @@ public class Boot
             {
                 extendClasspathWithJar(sysLoader, file);
             }
-        } 
+        }
     }
-
 }
