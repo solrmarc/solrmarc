solrmarc/solrmarc

commit 2b1d38425eabd7b6b32e1b50d4ef114a9a028a64

Deleting a bunch of code to build indexer object using StringReader rather than the lex and Java Cup parser

src/org/solrmarc/index/extractor/impl/fullrecord/FullRecordValueExtractorFactory.java
@@ -2,12 +2,10 @@ package org.solrmarc.index.extractor.impl.fullrecord;
 
 import org.solrmarc.index.extractor.AbstractValueExtractor;
 import org.solrmarc.index.extractor.AbstractValueExtractorFactory;
-import org.solrmarc.index.utils.StringReader;
 
 
 public class FullRecordValueExtractorFactory extends AbstractValueExtractorFactory
 {
-
     @Override
     public boolean canHandle(final String solrFieldName, final String mappingConfiguration)
     {
@@ -17,9 +15,7 @@ public class FullRecordValueExtractorFactory extends AbstractValueExtractorFacto
                 || mapping.startsWith("text");
     }
 
- 
-    
-    AbstractValueExtractor<?> createExtractor(final String solrFieldName, final String mapping)
+    public AbstractValueExtractor<?> createExtractor(final String solrFieldName, final String mapping)
     {
         if (mapping.startsWith("raw") || mapping.startsWith("FullRecordAsMARC".toLowerCase()))
         {
@@ -42,13 +38,6 @@ public class FullRecordValueExtractorFactory extends AbstractValueExtractorFacto
             return new FullRecordAsTextValueExtractor();
         }
         throw new IllegalArgumentException("Unknown impl configuration: " + solrFieldName + " = " + mapping);
-    }
-    
-    @Override
-    public AbstractValueExtractor<?> createExtractor(final String solrFieldName, final StringReader mappingConfiguration)
-    {
-        final String mapping = mappingConfiguration.readAll().toLowerCase();
-        return createExtractor(solrFieldName, mapping);
     }
 
     @Override

src/org/solrmarc/index/extractor/AbstractValueExtractorFactory.java
@@ -1,13 +1,8 @@
 package org.solrmarc.index.extractor;
 
-import org.solrmarc.index.utils.StringReader;
-
 public abstract class AbstractValueExtractorFactory
 {
-
     public abstract boolean canHandle(final String solrFieldName, final String mappingConfiguration);
-
-    public abstract AbstractValueExtractor<?> createExtractor(final String solrFieldName, final StringReader mappingConfiguration);
 
     public abstract AbstractValueExtractor<?> createExtractor(final String solrFieldName, final String[] parts);
 }

src/org/solrmarc/index/extractor/impl/constant/ConstantValueExtractorFactory.java
@@ -1,101 +1,18 @@
 package org.solrmarc.index.extractor.impl.constant;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.solrmarc.index.extractor.AbstractValueExtractor;
 import org.solrmarc.index.extractor.AbstractValueExtractorFactory;
-import org.solrmarc.index.utils.StringReader;
 
 public class ConstantValueExtractorFactory extends AbstractValueExtractorFactory
 {
-
     public boolean canHandle(final String solrFieldName, final String mappingConfiguration)
     {
         return mappingConfiguration.startsWith("\"") && mappingConfiguration.endsWith("\"");
     }
 
     @Override
-    public AbstractValueExtractor<?> createExtractor(final String solrFieldName, final StringReader mappingConfiguration)
-    {
-        final List<String> values = parseMappingConfiguration(mappingConfiguration.readAll());
-        if (values.isEmpty())
-        {
-            return null;
-        }
-        else
-        {
-            return new ConstantMultiValueExtractor(values);
-        }
-    }
-    
-    @Override
     public AbstractValueExtractor<?> createExtractor(String solrFieldName, String[] parts)
     {
         return new ConstantMultiValueExtractor(parts);
     }
-
-
-    private List<String> parseMappingConfiguration(final String mappingConfiguration)
-    {
-        final List<String> values = new ArrayList<>();
-        final char[] chars = mappingConfiguration.toCharArray();
-        boolean isQuoted = false;
-        boolean isEscaped = false;
-        StringBuilder buffer = new StringBuilder();
-        for (char c : chars)
-        {
-            switch (c) {
-                case '\t':
-                case ' ':
-                    if (isQuoted)
-                    {
-                        buffer.append(c);
-                    }
-                    break;
-                case '|':
-                    if (isQuoted)
-                    {
-                        buffer.append(c);
-                    }
-                    else
-                    {
-                        final String value = buffer.toString().trim();
-                        if (!value.isEmpty())
-                        {
-                            values.add(value);
-                        }
-                        buffer = new StringBuilder();
-                    }
-                    break;
-                case '\"':
-                    if (isEscaped)
-                    {
-                        isEscaped = false;
-                        buffer.append(c);
-                    }
-                    else
-                    {
-                        isQuoted = !isQuoted;
-                    }
-                    break;
-                case '\\':
-                    if (isEscaped)
-                    {
-                        buffer.append(c);
-                    }
-                    isEscaped = !isEscaped;
-                    break;
-                default:
-                    buffer.append(c);
-            }
-        }
-        if (!buffer.toString().isEmpty())
-        {
-            values.add(buffer.toString());
-        }
-        return values;
-    }
-
 }

src/org/solrmarc/index/extractor/impl/date/DateValueExtractorFactory.java
@@ -2,12 +2,10 @@ package org.solrmarc.index.extractor.impl.date;
 
 import org.solrmarc.index.extractor.AbstractValueExtractor;
 import org.solrmarc.index.extractor.AbstractValueExtractorFactory;
-import org.solrmarc.index.utils.StringReader;
 
 
 public class DateValueExtractorFactory extends AbstractValueExtractorFactory
 {
-
     @Override
     public boolean canHandle(final String solrFieldName, final String mappingConfiguration)
     {
@@ -19,25 +17,19 @@ public class DateValueExtractorFactory extends AbstractValueExtractorFactory
 
     public AbstractValueExtractor<?> createExtractor(final String solrFieldName, final String mapping)
     {
-        if (mapping.startsWith("dateRecordIndexed".toLowerCase()) || mapping.startsWith("index_date".toLowerCase()))
+        if (mapping.toLowerCase().startsWith("daterecordindexed") || mapping.toLowerCase().startsWith("index_date"))
         {
             return new DateRecordIndexedValueExtractor();
         }
-        else if (mapping.startsWith("date".toLowerCase()) || mapping.startsWith("dateOfPublication".toLowerCase()))
+        else if (mapping.toLowerCase().startsWith("date") || mapping.toLowerCase().startsWith("dateOfPublication"))
         {
             return new DateOfPublicationValueExtractor();
         }
-         
+
         throw new IllegalArgumentException("Unknown impl configuration: " + solrFieldName + " = " + mapping);
 
     }
-    @Override
-    public AbstractValueExtractor<?> createExtractor(final String solrFieldName, final StringReader mappingConfiguration)
-    {
-        final String mapping = mappingConfiguration.readAll().toLowerCase();
-        return createExtractor(solrFieldName, mapping);
 
-    }
     @Override
     public AbstractValueExtractor<?> createExtractor(final String solrFieldName, final String[] parts)
     {

src/org/solrmarc/index/extractor/impl/direct/DirectValueExtractorFactory.java
@@ -3,31 +3,14 @@ package org.solrmarc.index.extractor.impl.direct;
 import org.solrmarc.index.extractor.AbstractValueExtractor;
 import org.solrmarc.index.extractor.AbstractValueExtractorFactory;
 import org.solrmarc.index.specification.AbstractSpecificationFactory;
-import org.solrmarc.index.utils.StringReader;
 
 
 public class DirectValueExtractorFactory extends AbstractValueExtractorFactory
 {
-
     @Override
     public boolean canHandle(final String solrFieldName, final String indexConfiguration)
     {
         return (AbstractSpecificationFactory.canHandle(indexConfiguration));
-    }
-
-    @Override
-    public AbstractValueExtractor<?> createExtractor(final String solrFieldName, final StringReader indexConfiguration)
-    {
-//        int commaIndex = indexConfiguration.indexOf(',');
-//        if (commaIndex <= -1)
-//        {
-//            return new DirectMultiValueExtractor(indexConfiguration.readAll());
-//        }
-//        else
-//        {
-//            return new DirectMultiValueExtractor(indexConfiguration.readString(commaIndex));
-//        }
-        return null;
     }
 
     @Override

src/org/solrmarc/index/extractor/impl/patternMapping/PatternMappingValueExtractorFactory.java
@@ -2,24 +2,13 @@ package org.solrmarc.index.extractor.impl.patternMapping;
 
 
 import java.util.*;
-import java.util.regex.Pattern;
 
 import org.solrmarc.index.extractor.AbstractValueExtractor;
 import org.solrmarc.index.extractor.AbstractValueExtractorFactory;
-import org.solrmarc.index.utils.StringReader;
 
 public class PatternMappingValueExtractorFactory extends AbstractValueExtractorFactory
 {
     private static final Map<String, List<PatternMapping>> PATTERN_MAPPINGS = new HashMap<>();
-    private static final Pattern MAPPING_SPLITTING_PATTERN = Pattern.compile("=>");
-    private static final Comparator<PatternMapping> PATTERN_MAPPING_COMPARATOR = new Comparator<PatternMapping>()
-    {
-        @Override
-        public int compare(final PatternMapping o1, final PatternMapping o2)
-        {
-            return Integer.compare(o1.getOrderIndex(), o2.getOrderIndex());
-        }
-    };
 
     public static List<PatternMapping> getPatternMappingsForName(String name)
     {
@@ -50,40 +39,10 @@ public class PatternMappingValueExtractorFactory extends AbstractValueExtractorF
         return true;
     }
 
-    private void addPatternMapping(final String name, final PatternMapping mapping)
-    {
-        List<PatternMapping> mappings = getPatternMappingsForName(name);
-        mappings.add(mapping);
-        Collections.sort(mappings, PATTERN_MAPPING_COMPARATOR);
-    }
-
-    @Override
-    public AbstractValueExtractor<?> createExtractor(final String solrFieldName,
-            final StringReader mappingConfiguration)
-    {
-        final String patterns = mappingConfiguration.readAll();
-        final String[] split = MAPPING_SPLITTING_PATTERN.split(patterns, 2);
-        if (split.length != 2)
-        {
-            throw new IllegalArgumentException(
-                    "A pattern impl has to have two patterns divided by '=>'. " + solrFieldName + " = " + patterns);
-        }
-        final int index = solrFieldName.indexOf(".pattern_");
-        final String patternName = solrFieldName.substring(0, index);
-        final int patternIndex = Integer
-                .parseInt(solrFieldName.substring(index + ".pattern_".length(), solrFieldName.length()));
-        final PatternMapping patternMapping = new PatternMapping(split[0], split[1], patternIndex);
-        addPatternMapping(patternName, patternMapping);
-
-        // A pattern impl doesn't extract something, so no impl will be created.
-        return null;
-    }
-
     @Override
     public AbstractValueExtractor<?> createExtractor(String solrFieldName, String[] parts)
     {
         // TODO Auto-generated method stub
         return null;
     }
-
 }

src/org/solrmarc/index/extractor/impl/script/ScriptValueExtractorFactory.java
@@ -12,7 +12,6 @@ import org.solrmarc.index.extractor.methodcall.MethodCallContext;
 import org.solrmarc.index.extractor.methodcall.MethodCallMultiValueExtractor;
 import org.solrmarc.index.extractor.methodcall.MethodCallSingleValueExtractor;
 import org.solrmarc.index.indexer.ValueIndexerFactory;
-import org.solrmarc.index.utils.StringReader;
 import org.solrmarc.tools.PropertyUtils;
 
 import java.io.File;
@@ -118,13 +117,6 @@ public class ScriptValueExtractorFactory extends AbstractValueExtractorFactory
             return new MethodCallSingleValueExtractor(
                     new ScriptSingleValueMethodCall(interpreter, method, scriptFileName, context.getParameters().length), context.getParameters());
         }
-    }
-
-    @Override
-    public AbstractValueExtractor<?> createExtractor(final String solrFieldName, final StringReader mappingConfiguration)
-    {
-        MethodCallContext context = MethodCallContext.parseContextFromExtractorSpecification(mappingConfiguration);
-        return(createExtractor(solrFieldName, context));
     }
 
     @Override

src/org/solrmarc/index/extractor/methodcall/AbstractMethodCallFactory.java
@@ -11,7 +11,6 @@ import org.solrmarc.index.extractor.AbstractValueExtractor;
 import org.solrmarc.index.extractor.AbstractValueExtractorFactory;
 import org.solrmarc.index.indexer.IndexerSpecException;
 import org.solrmarc.index.indexer.ValueIndexerFactory;
-import org.solrmarc.index.utils.StringReader;
 
 public abstract class AbstractMethodCallFactory extends AbstractValueExtractorFactory
 {
@@ -221,13 +220,6 @@ public abstract class AbstractMethodCallFactory extends AbstractValueExtractorFa
             return new MethodCallSingleValueExtractor((SingleValueExtractorMethodCall) methodCall, context.getParameters());
         }
         return(null);
-    }
-
-    @Override
-    public AbstractValueExtractor<?> createExtractor(final String solrFieldName, final StringReader mappingConfiguration)
-    {
-        MethodCallContext context = MethodCallContext.parseContextFromExtractorSpecification(mappingConfiguration);
-        return createExtractor(solrFieldName, context);
     }
 
     @Override

commit 9076777078088f6d051449ba71bed320b15081e9

fix problem with full record caused by commit 2b1d38425eabd7b6b32e1b50d4ef114a9a028a64

src/org/solrmarc/index/extractor/impl/fullrecord/FullRecordValueExtractorFactory.java
@@ -17,23 +17,24 @@ public class FullRecordValueExtractorFactory extends AbstractValueExtractorFacto
 
     public AbstractValueExtractor<?> createExtractor(final String solrFieldName, final String mapping)
     {
-        if (mapping.startsWith("raw") || mapping.startsWith("FullRecordAsMARC".toLowerCase()))
+        String mappingLC = mapping.toLowerCase();
+        if (mappingLC.startsWith("raw") || mappingLC.startsWith("FullRecordAsMARC".toLowerCase()))
         {
             return new FullRecordAsMarcValueExtractor();
         }
-        else if (mapping.startsWith("xml") || mapping.startsWith("FullRecordAsXML".toLowerCase()))
+        else if (mappingLC.startsWith("xml") || mappingLC.startsWith("FullRecordAsXML".toLowerCase()))
         {
             return new FullRecordAsXMLValueExtractor();
         }
-        else if (mapping.startsWith("json2") || mapping.startsWith("FullRecordAsJSON2".toLowerCase()))
+        else if (mappingLC.startsWith("json2") || mappingLC.startsWith("FullRecordAsJSON2".toLowerCase()))
         {
             return new FullRecordAsJSON2ValueExtractor();
         }
-        else if (mapping.startsWith("json") || mapping.startsWith("FullRecordAsJSON".toLowerCase()))
+        else if (mappingLC.startsWith("json") || mappingLC.startsWith("FullRecordAsJSON".toLowerCase()))
         {
             return new FullRecordAsJSONValueExtractor();
         }
-        else if (mapping.startsWith("text") || mapping.startsWith("FullRecordAsText".toLowerCase()))
+        else if (mappingLC.startsWith("text") || mappingLC.startsWith("FullRecordAsText".toLowerCase()))
         {
             return new FullRecordAsTextValueExtractor();
         }