f1ca70408e7c87010984357f9fdb12dff15540c7
Oliver Obenland <oliver.obenland@uni-tuebingen.de>
2016-12-21 08:25:08 +0100

    Removing unnecessary changes

--- src/org/solrmarc/index/utils/FastClasspathUtils.java
+++ src/org/solrmarc/index/utils/FastClasspathUtils.java
@@ -27,7 +27,7 @@ public class FastClasspathUtils
         extractors = new LinkedHashSet<>();
         mappers = new LinkedHashSet<>();
         mixins = new LinkedHashSet<>();
-        FastClasspathScanner scanner = new FastClasspathScanner("org")
+        FastClasspathScanner scanner = new FastClasspathScanner("org")  
             .matchSubclassesOf(AbstractValueExtractorFactory.class, new SubclassMatchProcessor<AbstractValueExtractorFactory>() 
             {
                 @Override
