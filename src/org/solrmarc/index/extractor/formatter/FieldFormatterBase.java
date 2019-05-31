6a7a807a16516e6df407558e138488c8590a33b2
Robert Haschart <rh9ec@virginia.edu>
2019-05-31 14:01:32 -0400

    Fix unintended change in how field formatting is handled caused by adding new functionality

--- src/org/solrmarc/index/extractor/formatter/FieldFormatterBase.java
+++ src/org/solrmarc/index/extractor/formatter/FieldFormatterBase.java
@@ -398,6 +398,10 @@ public class FieldFormatterBase implements FieldFormatter
         }
         String ind1 = (vf instanceof DataField) ? ""+ ((DataField)vf).getIndicator1() : " ";
         String ind2 = (vf instanceof DataField) ? ""+ ((DataField)vf).getIndicator2() : " ";
+        if (pattern.contains("$"+sfCode))
+        {
+            pattern = pattern .replace("$"+sfCode, mappedDataVal);
+        }
         pattern = pattern.replace("%tag", vf.getTag()).replace("%1", ind1).replace("%2", ind2).replace("%sf", sfCode).replace("%d", mappedDataVal);
         return(pattern);
     }
